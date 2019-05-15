d458d46283bed1a254f862e58452d70cd5d7d257
youssef nahhas <nahhas.youssef@gmail.com>
2019-05-15 16:25:50 +0200

    End of section 6

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -38,9 +38,7 @@ namespace DatingApp.API.Controllers {
         }
 
         [HttpPost ("login")]
-        public async Task<IActionResult> Login (UserForLoginDto userForLoginDto) {
-
-            throw new Exception ("Computer says no!");
+        public async Task<IActionResult> Login (UserForLoginDto userForLoginDto) {            
 
             var userFromRepo = await _repo.Login (userForLoginDto.Username, userForLoginDto.Password);
 
